fdd653a026e95134f8dfb6ba251a07f0bfc2c029
Shivam Kumar <kshivam0508@gmail.com>
2025-09-03 21:30:29 +0530

    Python/Operators/logical.py

---
+++ Python/Operators/logical.py
@@ -0,0 +1,11 @@
+a=4
+b=4
+print(a and b) # 3
+print(a or b) # 2
+print(not a) # False
+print(not b) # Fals
+print((3>4) and (4<3)) # False
+print(not(3>4) or (4<3))
+print(not(3>4) and (4<3)) # False
+
+print(not((3>4) and (4<3))) # False
