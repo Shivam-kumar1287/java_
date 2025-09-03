2923fef420e5d9c39de4ca470dcdaf60674ef0ae
Shivam Kumar <kshivam0508@gmail.com>
2025-09-03 21:19:11 +0530

    Python/Operators/

---
+++ Python/Operators/arthematic.py
@@ -0,0 +1,9 @@
+a=3
+b=3
+print(a+b,"addition") #addition
+print(a-b,"subtraction") #subtraction
+print(a*b,"multiplication") #multiplication
+print(a/b,"division") #division
+print(a%b,"modulus") #modulus
+print(a**b,"exponential") #exponential
+print(a//b,"floor division") #floor division
