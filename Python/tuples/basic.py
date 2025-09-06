8cd7c4ed8549f3dd519a84f901c2e3e51ccbbb12
Shivam Kumar <kshivam0508@gmail.com>
2025-09-07 01:10:11 +0530

    tuples

---
+++ Python/tuples/basic.py
@@ -0,0 +1,6 @@
+a="hjdf"
+print(type(a))
+b="sdfs","sdff"
+print(type(b))
+print(b)
+print(b[0])
---
+++ Python/tuples/slicing.py
@@ -0,0 +1,13 @@
+a="onew","two","three","four","five"
+print(a)
+print(type(a))
+print(a[0:3])
+print(a[1:])
+print(a[:4])
+print(a[-1])
+print(a[-4:-1])
+print("revrse\n")
+print(a[::-1])
+
+for i in a:
+    print(i,end=" ")
