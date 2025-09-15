e953e1168f98ba8fd3780a65d665523dade3d9ba
Shivam Kumar <kshivam0508@gmail.com>
2025-09-15 12:18:54 +0530

    Python/lamdba and datetime

---
+++ Python/datetime/basic1.py
@@ -0,0 +1,10 @@
+import datetime
+
+x = datetime.datetime.now()
+
+print(x)             # full date + time
+print(x.date())      # only date
+print(x.strftime("%x"))  # local version of date (MM/DD/YY format)
+print(x.strftime("%f"))
+print(x.strftime("%z"))
+print(x.strftime("%B - %Y %I-%p "))
--- Python/lamdba/basic1.py
+++ Python/lamdba/basic1.py
@@ -3,3 +3,6 @@ def myfunc(a,b):
 a=int(input("enter a \n"))
 b=int(input("enter b \n"))
 print(myfunc(a,b))
+def myfunc1(a,b):
+    return (lambda a,b:a+b)(a,b)
+print(myfunc1(a,b))
