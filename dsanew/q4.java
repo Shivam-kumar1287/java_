bfb26750a0126ae11f3e93187158ffa57da48751
Shivam Kumar <kshivam0508@gmail.com>
2025-10-13 18:48:19 +0530

    dsanew/q4.java

--- dsanew/q4.java
+++ dsanew/q4.java
@@ -2,16 +2,17 @@ import java.util.*;
 public  class q4 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int chocolate=sc.nextInt();
+      int chocolate=sc.nextInt();
         int students=sc.nextInt();
         int postion=sc.nextInt();
-        int current=postion;
+         int current=postion;
 
         // distribute chocolates
         //formula int lastStudent=(postion+chocolate-1)%students;
         // if(lastStudent==0) lastStudent=students;
-        // System.out.println(lastStudent);
         
+        // System.out.println(lastStudent);
+
         while(chocolate>0){
            chocolate--;   // give one chocolate
             current++;      // move to next student
